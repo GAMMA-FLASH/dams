08819ed570a40933d07c42d0ea82f9b4c18abbce
Antonio Addis <antonio.addis@inaf.it>
2021-12-02 12:57:48 +0100

    cleaning hdf5create

--- gui/hdf5create.py
+++ gui/hdf5create.py
@@ -89,80 +89,24 @@ class Hdf5Create():
             arraysy._v_attrs.configID = wf.configID[0]
             arraysy._v_attrs.calib = 8
 
-            size_arrays = sys.getsizeof(wf.sigt) + sys.getsizeof(wf.sige)
-            self.logger.warning(f"sizeof array is {size_arrays}")
-
             arraysy[:16384, :2] = np.transpose(np.array([wf.sigt,wf.sige*-1]))[:16384, :2]
 
-            if i == 1: #we select only waveform 1 for quicklook analysis in gui
-                dict_wf = {"x": wf.sigt, "y": (wf.sige*-1)}
-                json_wf = json.dumps(dict_wf)
+            try: 
+                if i == 1: #we select only waveform 1 for quicklook analysis in gui
+                    dict_wf = {"x": wf.sigt.tolist(), "y": (wf.sige*-1).tolist()}
+                    json_wf = json.dumps(dict_wf)
 
-                query = f"UPDATE gammaflash_test.waveform_dl1 SET waveform='{json_wf}' WHERE RedpitayaID=1;"
+                    query = f"UPDATE gammaflash_test.waveform_dl1 SET waveform='{json_wf}' WHERE RedpitayaID=1;"
 
-                self.cursor.execute(query)
+                    self.cursor.execute(query)
 
-                self.conn.commit()
+                    self.conn.commit()
+            except Exception as e:
+                self.logger.critical(f"{e}")
         
         h5file.close()
         okfile = open(f"{filename}.ok", "w")
-        #self.wfTotCount = 0
         self.waveforms = []
-
-        
-
-if __name__ == "__main__":
-    pass
-    """
-    filedir = sys.argv[1]
-
-    files = glob.glob(f"{filedir}/*.bin")
-
-    count = 0
-
-    for i,filein in enumerate(files):
-
-
-        if count == 0:
-            h5file = open_file(f"dl0_{time()}.h5", mode="w", title="dl0")
-
-            group = h5file.create_group("/", 'waves', 'waves information')
-            atom = tables.Float32Atom()
-            shape = (16384, 2)
-            filters = tables.Filters(complevel=5, complib='zlib')
-        try:
-            x, y = redPitayaDataConverter(filein)
-        except:
-            continue
-        filename = filein.split("/")[-1]
-
-        arraysy = h5file.create_carray(group, f'{filename}', atom, shape, f"{filename}", filters=filters)
-        arraysy._v_attrs.rp_id = 1
-        arraysy._v_attrs.tstart = "2021-08-25T16:00:04+00:00"
-        arraysy._v_attrs.tend = "2021-08-25T17:00:04+00:00"
-        arraysy._v_attrs.calib = 8
-
-        arraysy[:16384, :2] = np.transpose(np.array([x,y]))[:16384, :2]
-
-        count +=1
-
-        if count == 10000:
-            h5file.close()
-            count = 0
-
-        #arraysx = h5file.create_array(group, f"{filein}_y", np.array(x), f"{filein}_y")
-    
-        table = h5file.create_table(group, f'{filename}', GfData, f"{filename}", expectedrows=16500)
-
-        gfData = table.row
-        for i in range(len(x)):
-            gfData["x"] = x[i]
-            gfData["y"] = y[i]
-
-            gfData.append()
-    
-    table.flush
-    """
 
 
 
