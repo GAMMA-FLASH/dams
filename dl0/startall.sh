22a86c15eda364fe986a20162142c109f0c32a7c
lucacastaldini <luca.castaldini96@gmail.com>
2023-12-18 23:12:32 +0100

    telegraf and sh

---
+++ dl0/startall.sh
@@ -0,0 +1,7 @@
+
+nohup python gfcl.py --addr 192.168.1.101 --port 1234 --outdir $ODIR/RPG101/35mV/ --wformno 1000 &
+nohup python gfcl.py --addr 192.168.1.102 --port 1234 --outdir $ODIR/RPG102/35mV/ --wformno 1000 &
+nohup python gfcl.py --addr 192.168.1.103 --port 1234 --outdir $ODIR/RPG103/35mV/ --wformno 1000 &
+nohup python gfcl.py --addr 192.168.1.104 --port 1234 --outdir $ODIR/RPG104/35mV/ --wformno 1000 &
+nohup python gfcl.py --addr 192.168.1.105 --port 1234 --outdir $ODIR/RPG105/35mV/ --wformno 1000 &
+nohup python gfcl.py --addr 192.168.1.106 --port 1234 --outdir $ODIR/RPG106/35mV/ --wformno 1000 &
