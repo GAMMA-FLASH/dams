4c28213f91b5ba3f583415e56f937da522f554c6
lucacastaldini <luca.castaldini96@gmail.com>
2024-06-03 12:24:14 +0200

    fix bugs

--- setup/gfpl/startall.sh
+++ setup/gfpl/startall.sh
@@ -27,16 +27,17 @@ else
     echo "gfcl.ini not defined"
     exit 1
 fi
-if [ "$(hostname)" = "gamma-flash.iasfbo.inaf.it" ]; then
+if [ "$(cat /etc/hostname)" = "gamma-flash.iasfbo.inaf.it" ]; then
     # Source the virtual environment (uncomment if needed)
     # source ~/venvs/gammaflash-influx/bin/activate
     PYTHON=python3.9
 else
+    source activate $CONDA_ENV_NAME
     PYTHON=python
 fi
 
 export PYTHONUNBUFFERED=yes
-nohup $PYTHON $GFCL --addr 192.168.1.101 --port 1234 --outdir $ODIR/RPG101/35mV/ --wformno 1000 > $DL0_LOGS//gfcl_RP101.log &
+nohup $PYTHON $GFCL --addr 192.168.1.101 --port 1234 --outdir $ODIR/RPG101/35mV/ --wformno 1000 > $DL0_LOGS/gfcl_RP101.log &
 nohup $PYTHON $GFCL --addr 192.168.1.102 --port 1234 --outdir $ODIR/RPG102/35mV/ --wformno 1000 > $DL0_LOGS/gfcl_RP102.log &
 nohup $PYTHON $GFCL --addr 192.168.1.103 --port 1234 --outdir $ODIR/RPG103/35mV/ --wformno 1000 > $DL0_LOGS/gfcl_RP103.log &
 nohup $PYTHON $GFCL --addr 192.168.1.104 --port 1234 --outdir $ODIR/RPG104/35mV/ --wformno 1000 > $DL0_LOGS/gfcl_RP104.log &
