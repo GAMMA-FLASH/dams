745e81dbc20795de90be07eb9abf7740573dab23
lucacastaldini <luca.castaldini96@gmail.com>
2024-10-15 12:19:24 +0200

    add deployment for lab tests

---
+++ setup/testlab/bootstrap2gen_ip.sh
@@ -0,0 +1,15 @@
+# source /opt/gflashenv/bin/activate
+
+bootstrap() {
+    HOST="$1"
+    echo -e "\e[32m================ RPG at addr: $HOST ================\e[0m"
+    local HOST=$1
+    current_time=$(date -u "+%Y-%m-%d %H:%M:%S")  ## take UTC timestamp
+    ssh -tt $HOST << EOF
+        date -s '$current_time' ;
+        bash bootstrap2.sh ;
+        exit
+EOF
+}
+
+bootstrap $1
---
+++ setup/testlab/startone.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+PIDS=gfcl.pids
+
+CONDA_ENV_NAME="gammaflash"
+# Change to script directory:
+GAMMAFLASH_DAMS_PATH=$(dirname $(dirname $(dirname "$(realpath "$0")")))
+cd $GAMMAFLASH_DAMS_PATH/dl0
+
+# Check if ODIR is defined
+if [ -z "$ODIR" ]; then
+    echo "ODIR not defined"
+    exit 1
+fi
+
+# Check if DAMS is defined
+if [ -z "$DAMS" ]; then
+    echo "DAMS root not defined"
+    exit 1
+fi
+
+GFCL=${GFCL:-"$DAMS/dl0/gfcl.py"}
+echo "Using Client Script: ${GFCL}"
+
+# Check if gfcl.ini exists
+if [ -e "gfcl.ini" ]; then
+    echo "Using gfcl.ini: $(readlink -f gfcl.ini)"
+else
+    echo "gfcl.ini not defined"
+    exit 1
+fi
+
+# Check if the input is between 101 and 106
+
+if [ "$(cat /etc/hostname)" = "gamma-flash.iasfbo.inaf.it" ]; then
+    # Source the virtual environment (uncomment if needed)
+    # source ~/venvs/gammaflash-influx/bin/activate
+    PYTHON=python3.9
+else
+    source activate $CONDA_ENV_NAME
+    PYTHON=python
+fi
+export PYTHONUNBUFFERED=yes
+
+# $PYTHON $GFCL --addr $IP_ADDRESS --port 1234 --outdir $ODIR/RPG$IP_LAST_OCTET/35mV/ --wformno 1000
+$PYTHON $GFCL --addr $1 --port 1234 --outdir $ODIR/RPG202/35mV/ --wformno 1000
