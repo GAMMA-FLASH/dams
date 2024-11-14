e32e8a20c5b1ce5391cb09c9528841a76ffd0f30
lucacastaldini <luca.castaldini96@gmail.com>
2024-11-14 11:32:21 +0100

    save new docker container entrypoint

--- env/entrypoint.sh
+++ env/entrypoint.sh
@@ -1,3 +1,29 @@
+#!/bin/bash
+
+# Nome del vecchio e del nuovo ambiente
+condaenviron="gammaflash"
 cd $HOME
-source activate gammaflash
-PYTHONPATH="/home/gammaflash/analyze_acquisizioni/rta-dataprocessor/workers"
+echo "starting gammasky-container"
+source activate $condaenviron
+
+#!/bin/bash
+
+TOKEN="gf2023#"
+
+echo "starting notebook. Use Token: $TOKEN"
+# Avviare il notebook Jupyter, legandolo a tutte le interfacce di rete e mettendo i log su stdout
+nohup jupyter-lab --ip="*" --no-browser --autoreload --NotebookApp.token="$TOKEN" --notebook-dir=/home/usergamma/dams --allow-root > /dev/stdout 2>&1 &
+
+# Verifica se le variabili sono già presenti
+if ! grep -q "export DAMS=" ~/.bashrc; then
+    echo "exporting variables"
+    echo 'export DAMS="/home/usergamma/dams"' >> ~/.bashrc
+    echo 'export RPG_CONFIG="$DAMS/setup/testlab"' >> ~/.bashrc
+else
+    echo "Le variabili di ambiente sono già presenti in .bashrc."
+fi
+
+# Mantieni il container in esecuzione (per evitare che termini subito)
+echo "Done"
+tail -f /dev/null
+
